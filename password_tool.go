abfc0379f4c2d6ce7480beec426a8d57a2e8b99d
Kaialogen <kaialogen@protonmail.com>
2023-04-19 23:13:09 +0100

    fixes to gui

--- password_tool.go
+++ password_tool.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"math/rand"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 	"unicode"
@@ -61,6 +61,21 @@ func main() {
 		checkPasswordStrength(password)
 	})
 
+	// Add a label for the generated password
+	genLabel, err := gtk.LabelNew("")
+	if err != nil {
+		log.Fatal("Unable to create label:", err)
+	}
+	grid.Attach(genLabel, 0, 2, 2, 1)
+
+	// Add a text field for the password length
+	genEntry, err := gtk.EntryNew()
+	if err != nil {
+		log.Fatal("Unable to create entry:", err)
+	}
+	genEntry.SetText("8")
+	grid.Attach(genEntry, 1, 3, 1, 1)
+
 	// Add a button to generate password
 	genButton, err := gtk.ButtonNewWithLabel("Generate")
 	if err != nil {
@@ -73,27 +88,12 @@ func main() {
 		genLabel.SetText(password)
 	})
 
-	// Add a label for the generated password
-	genLabel, err := gtk.LabelNew("")
-	if err != nil {
-		log.Fatal("Unable to create label:", err)
-	}
-	grid.Attach(genLabel, 0, 2, 2, 1)
-
 	// Add a label for the password length field
 	lenLabel, err := gtk.LabelNew("Length:")
 	if err != nil {
 		log.Fatal("Unable to create label:", err)
 	}
 	grid.Attach(lenLabel, 0, 3, 1, 1)
-
-	// Add a text field for the password length
-	genEntry, err := gtk.EntryNew()
-	if err != nil {
-		log.Fatal("Unable to create entry:", err)
-	}
-	genEntry.SetText("8")
-	grid.Attach(genEntry, 1, 3, 1, 1)
 
 	// Show the window
 	win.ShowAll()
@@ -113,57 +113,56 @@ func checkPasswordStrength(password string) {
 	score += checkRepeatingChars(password)
 	score += checkSequentialChars(password)
 
-var strength string
-if score >= 6 {
-	strength = "strong"
-} else if score >= 4 {
-	strength = "medium"
-} else {
-	strength = "weak"
-}
+	var strength string
+	if score >= 6 {
+		strength = "strong"
+	} else if score >= 4 {
+		strength = "medium"
+	} else {
+		strength = "weak"
+	}
 
-dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_INFO, gtk.BUTTONS_CLOSE, "Password strength: %s", strength)
-dialog.Run()
-dialog.Destroy()
-} 
+	dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_INFO, gtk.BUTTONS_CLOSE, "Password strength: %s", strength)
+	dialog.Run()
+	dialog.Destroy()
+}
 
 func checkPasswordLength(password string) int {
 	if len(password) < 8 {
-	dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, "Password is too short")
-	dialog.Run()
-	dialog.Destroy()
-	return 0
-	}
-	return 1
-	}
-	
-	func checkLowerCase(password string) int {
-	if !strings.ContainsAny(password, "abcdefghijklmnopqrstuvwxyz") {
-	dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, "Password does not contain any lowercase characters")
-	dialog.Run()
-	dialog.Destroy()
-	return 0
+		dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, "Password is too short")
+		dialog.Run()
+		dialog.Destroy()
+		return 0
 	}
 	return 1
 }
 
+func checkLowerCase(password string) int {
+	if !strings.ContainsAny(password, "abcdefghijklmnopqrstuvwxyz") {
+		dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, "Password does not contain any lowercase characters")
+		dialog.Run()
+		dialog.Destroy()
+		return 0
+	}
+	return 1
+}
 
 func checkUpperCase(password string) int {
 	if !strings.ContainsAny(password, "ABCDEFGHIJKLMNOPQRSTUVWXYZ") {
-	dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, "Password does not contain any uppercase characters")
-	dialog.Run()
-	dialog.Destroy()
-	return 0
+		dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, "Password does not contain any uppercase characters")
+		dialog.Run()
+		dialog.Destroy()
+		return 0
 	}
 	return 1
 }
 
 func checkNumeric(password string) int {
 	if !strings.ContainsAny(password, "0123456789") {
-	dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, "Password does not contain any numeric characters")
-	dialog.Run()
-	dialog.Destroy()
-	return 0
+		dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, "Password does not contain any numeric characters")
+		dialog.Run()
+		dialog.Destroy()
+		return 0
 	}
 	return 1
 }
@@ -171,10 +170,10 @@ func checkNumeric(password string) int {
 func checkSpecialChars(password string) int {
 	specialChars := "~`!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?"
 	if !strings.ContainsAny(password, specialChars) {
-	dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, "Password does not contain any special characters")
-	dialog.Run()
-	dialog.Destroy()
-	return 0
+		dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, "Password does not contain any special characters")
+		dialog.Run()
+		dialog.Destroy()
+		return 0
 	}
 	return 1
 }
@@ -182,7 +181,7 @@ func checkSpecialChars(password string) int {
 func checkDictionaryWords(password string) int {
 	file, err := os.Open("wordlists/rockyou.txt")
 	if err != nil {
-	log.Fatal(err)
+		log.Fatal(err)
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
@@ -190,7 +189,7 @@ func checkDictionaryWords(password string) int {
 
 	var dictionary []string
 	for scanner.Scan() {
-	dictionary = append(dictionary, scanner.Text())
+		dictionary = append(dictionary, scanner.Text())
 	}
 
 	for _, word := range dictionary {
@@ -207,37 +206,37 @@ func checkDictionaryWords(password string) int {
 
 func checkRepeatingChars(password string) int {
 	for i := 0; i < len(password)-2; i++ {
-	if password[i] == password[i+1] && password[i+1] == password[i+2] {
-	dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, "Password contains repeating characters")
-	dialog.Run()
-	dialog.Destroy()
-	return 0
-	}
+		if password[i] == password[i+1] && password[i+1] == password[i+2] {
+			dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, "Password contains repeating characters")
+			dialog.Run()
+			dialog.Destroy()
+			return 0
+		}
 	}
 	return 1
 }
 
 func checkSequentialChars(password string) int {
 	for i := 0; i < len(password)-2; i++ {
-	if unicode.IsLetter(rune(password[i])) && unicode.IsLetter(rune(password[i+1])) && unicode.IsLetter(rune(password[i+2])) {
-		if password[i]+1 == password[i+1] && password[i+1]+1 == password[i+2] {
-		dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, "Password contains sequential characters")
-		dialog.Run()
-		dialog.Destroy()
-		return 0
+		if unicode.IsLetter(rune(password[i])) && unicode.IsLetter(rune(password[i+1])) && unicode.IsLetter(rune(password[i+2])) {
+			if password[i]+1 == password[i+1] && password[i+1]+1 == password[i+2] {
+				dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, "Password contains sequential characters")
+				dialog.Run()
+				dialog.Destroy()
+				return 0
+			}
 		}
-		}
-		}
-		return 1
+	}
+	return 1
 }
 
 func generatePassword(lengthStr string) string {
 	length, err := strconv.Atoi(lengthStr)
 	if err != nil || length < 8 {
-	dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, "Invalid password length")
-	dialog.Run()
-	dialog.Destroy()
-	return ""
+		dialog := gtk.MessageDialogNew(nil, gtk.DIALOG_MODAL, gtk.MESSAGE_WARNING, gtk.BUTTONS_CLOSE, "Invalid password length")
+		dialog.Run()
+		dialog.Destroy()
+		return ""
 	}
 	rand.Seed(time.Now().UnixNano())
 
