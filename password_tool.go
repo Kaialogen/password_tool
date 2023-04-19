8352fd6f9fc4fc16cb43dde15e530abf12b1f96e
Kaialogen <kaialogen@protonmail.com>
2023-04-19 22:28:54 +0100

    scoring system

--- password_tool.go
+++ password_tool.go
@@ -14,50 +14,67 @@ func main() {
 	var password string
 	fmt.Scanln(&password)
 
-	checkPasswordLength(password)
-	checkLowerCase(password)
-	checkUpperCase(password)
-	checkNumeric(password)
-	checkSpecialChars(password)
-	checkDictionaryWords(password)
-	checkRepeatingChars(password)
-	checkSequentialChars(password)
+	score := 0
+	score += checkPasswordLength(password)
+	score += checkLowerCase(password)
+	score += checkUpperCase(password)
+	score += checkNumeric(password)
+	score += checkSpecialChars(password)
+	score += checkDictionaryWords(password)
+	score += checkRepeatingChars(password)
+	score += checkSequentialChars(password)
 
-	fmt.Println("Password strength check complete")
+	if score >= 6 {
+		fmt.Println("Password is strong")
+	} else if score >= 4 {
+		fmt.Println("Password is medium")
+	} else {
+		fmt.Println("Password is weak")
+	}
 }
 
-func checkPasswordLength(password string) {
+func checkPasswordLength(password string) int {
 	if len(password) < 8 {
 		fmt.Println("Password is too short")
+		return 0
 	}
+	return 1
 }
 
-func checkLowerCase(password string) {
+func checkLowerCase(password string) int {
 	if !strings.ContainsAny(password, "abcdefghijklmnopqrstuvwxyz") {
 		fmt.Println("Password does not contain any lowercase characters")
+		return 0
 	}
+	return 1
 }
 
-func checkUpperCase(password string) {
+func checkUpperCase(password string) int {
 	if !strings.ContainsAny(password, "ABCDEFGHIJKLMNOPQRSTUVWXYZ") {
 		fmt.Println("Password does not contain any uppercase characters")
+		return 0
 	}
+	return 1
 }
 
-func checkNumeric(password string) {
+func checkNumeric(password string) int {
 	if !strings.ContainsAny(password, "0123456789") {
 		fmt.Println("Password does not contain any numeric characters")
+		return 0
 	}
+	return 1
 }
 
-func checkSpecialChars(password string) {
+func checkSpecialChars(password string) int {
 	specialChars := "~`!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?"
 	if !strings.ContainsAny(password, specialChars) {
 		fmt.Println("Password does not contain any special characters")
+		return 0
 	}
+	return 1
 }
 
-func checkDictionaryWords(password string) {
+func checkDictionaryWords(password string) int {
 	file, err := os.Open("wordlists/rockyou.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -75,27 +92,30 @@ func checkDictionaryWords(password string) {
 	for _, word := range dictionary {
 		if strings.Contains(strings.ToLower(password), word) {
 			fmt.Println("Password contains a dictionary word")
-			break
+			return 0
 		}
 	}
+	return 1
 }
 
-func checkRepeatingChars(password string) {
+func checkRepeatingChars(password string) int {
 	for i := 0; i < len(password)-2; i++ {
 		if password[i] == password[i+1] && password[i+1] == password[i+2] {
 			fmt.Println("Password contains repeating characters")
-			break
+			return 0
 		}
 	}
+	return 1
 }
 
-func checkSequentialChars(password string) {
+func checkSequentialChars(password string) int {
 	for i := 0; i < len(password)-2; i++ {
 		if unicode.IsLetter(rune(password[i])) && unicode.IsLetter(rune(password[i+1])) && unicode.IsLetter(rune(password[i+2])) {
 			if password[i]+1 == password[i+1] && password[i+1]+1 == password[i+2] {
 				fmt.Println("Password contains sequential characters")
-				break
+				return 0
 			}
 		}
 	}
+	return 1
 }
